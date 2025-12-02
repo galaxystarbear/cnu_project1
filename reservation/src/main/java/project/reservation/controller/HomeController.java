c2de07aa3301697abd4755808f0c7451e891d02f
planetHan <blue1hjw@gmail.com>
2025-12-02 16:38:15 +0900

    더미추가

--- reservation/src/main/java/project/reservation/controller/HomeController.java
+++ reservation/src/main/java/project/reservation/controller/HomeController.java
@@ -2,6 +2,10 @@ package project.reservation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import project.reservation.domain.Time;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -10,4 +14,5 @@ public class HomeController {
     public String home() {
         return "login";
     }
+
 }
---
+++ reservation/src/main/java/project/reservation/domain/UserDto.java
@@ -0,0 +1,22 @@
+package project.reservation.domain;
+
+public class UserDto {
+    private String userid;
+    private String pass;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
